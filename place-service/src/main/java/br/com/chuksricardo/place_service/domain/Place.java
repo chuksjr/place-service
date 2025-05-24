c80a322382a5c4b6501ec4d1a876742309bef75e
Chuks Ricardo Iloegbunam <chuksricardo@gmail.com>
2025-05-24 17:58:12 -0300

    Definição do Dominio

---
+++ place-service/src/main/java/br/com/chuksricardo/place_service/domain/Place.java
@@ -0,0 +1,7 @@
+package br.com.chuksricardo.place_service.domain;
+
+import java.time.LocalDateTime;
+
+public record Place(Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+   
+}
