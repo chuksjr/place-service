091ede517b73ad3560065d70f3a3e2fc5fbdf0de
Chuks Ricardo Iloegbunam <chuksricardo@gmail.com>
2025-05-24 18:59:09 -0300

    Configuração de Auditoria

--- place-service/src/main/java/br/com/chuksricardo/place_service/config/PlaceConfig.java
+++ place-service/src/main/java/br/com/chuksricardo/place_service/config/PlaceConfig.java
@@ -2,11 +2,13 @@ package br.com.chuksricardo.place_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 import br.com.chuksricardo.place_service.domain.PlaceRepository;
 import br.com.chuksricardo.place_service.domain.PlaceService;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
   
   @Bean
