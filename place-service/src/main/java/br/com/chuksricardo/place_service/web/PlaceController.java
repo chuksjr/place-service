024b06fbe93a25999e5fa67fa188a4a06eca4637
Chuks Ricardo Iloegbunam <chuksricardo@gmail.com>
2025-05-24 18:12:41 -0300

    Criação do controller

---
+++ place-service/src/main/java/br/com/chuksricardo/place_service/web/PlaceController.java
@@ -0,0 +1,28 @@
+package br.com.chuksricardo.place_service.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.chuksricardo.place_service.domain.Place;
+import br.com.chuksricardo.place_service.domain.PlaceService;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+  private PlaceService placeService;
+
+  public PlaceController(PlaceService placeService) {
+    this.placeService = placeService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Mono<Place>> create(Place place){
+    var createdPlace = placeService.create(place);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
+  }
+
+}
